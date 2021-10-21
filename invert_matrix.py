c7cd5e16cdf8aa31bd1b6e38a4b1309f27630543
Ryan Christopher Bahillo <67821138+xtnctx@users.noreply.github.com>
2021-10-21 18:03:00 +0800

    Add files via upload

---
+++ invert_matrix.py
@@ -0,0 +1,24 @@
+m = [[0,0,0],
+     [0,0,0],
+     [0,0,0,0],
+     [0,0,0,0]]
+
+n = [[1,1,1],
+     [1,1,1],
+     [1,1,1]]
+
+""" invert a 2D matirx """
+def invert(matrix):
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            if matrix[row][column] == 0:
+                matrix[row][column] = 1
+            else:
+                matrix[row][column] = 0
+    return matrix
+    
+
+
+print(invert(n))
+        
+
